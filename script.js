8badea55d95590465f9e155374fcff101ad8fb35
SergieCode <sergiosnsdk@gmail.com>
2023-05-20 22:23:32 -0300

    Se agregó Javascript para búsqueda de películas con loading

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById('searchButton').addEventListener('click', searchMovies)
+
+let api_key = 'acf5472e45e436d5026bfd4c5e792981'
+let urlBase = 'https://api.themoviedb.org/3/search/movie'
+let urlImg = 'https://image.tmdb.org/t/p/w200'
+
+let resultContainer = document.getElementById('results')
+
+function searchMovies(){
+    resultContainer.innerHTML = 'Cargando...'
+    let searchInput = document.getElementById('searchInput').value
+    fetch(`${urlBase}?api_key=${api_key}&query=${searchInput}`)
+    .then(response => response.json())
+    .then(response => displayMovies(response.results))
+}
+
+
+function displayMovies(movies){
+    resultContainer.innerHTML = ''
+
+    if(movies.length === 0){
+        resultContainer.innerHTML= '<p>No se encontraron resultados para tu búsqueda </p>'
+        return
+    }
+
+    movies.forEach(movie => {
+        let movieDiv = document.createElement('div')
+        movieDiv.classList.add('movie')
+
+        let title = document.createElement('h2')
+        title.textContent = movie.title
+
+        let releaseDate = document.createElement('p')
+        releaseDate.textContent = 'La fecha de lanzamiento fue: ' + movie.release_date
+
+        let overview = document.createElement('p')
+        overview.textContent = movie.overview
+
+        let posterPath = urlImg + movie.poster_path
+        let poster = document.createElement('img')
+        poster.src = posterPath
+
+        movieDiv.appendChild(poster)
+        movieDiv.appendChild(title)
+        movieDiv.appendChild(releaseDate)
+        movieDiv.appendChild(overview)
+
+        resultContainer.appendChild(movieDiv)
+
+    })
+
+}
+
